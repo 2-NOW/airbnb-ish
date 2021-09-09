4d072c349fcd6caffdefa01c551dd8d827a1b6ce
2-NOW <kj109888@gmail.com>
2021-09-09 14:07:56 +0900

    Header1

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,18 @@
-import Head from "next/head";
-import Header from "../components/Header";
-import Hero from "../components/Hero";
-import Explore from "../components/Explore";
-import Banner from "../components/Banner";
-import Cards from "../components/Cards";
-import Hosting from "../components/Hosting";
-import Footer from "../components/Footer";
-import { live, discover } from "../data";
+import Head from 'next/head';
+import Header from '../components/Header1';
+import Hero from '../components/Hero';
+import Explore from '../components/Explore';
+import Banner from '../components/Banner';
+import Cards from '../components/Cards';
+import Hosting from '../components/Hosting';
+import Footer from '../components/Footer';
+import { live, discover } from '../data';
 
 export default function Home() {
   return (
     <>
       <Header />
-
+      {/* 
       <main>
         <Hero />
         <Explore />
@@ -22,7 +22,7 @@ export default function Home() {
         <Hosting />
       </main>
 
-      <Footer />
+      <Footer /> */}
     </>
   );
 }
